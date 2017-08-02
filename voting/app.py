25d0e0c98aee0dcd18f9a640635f7d46cb46114b
Poe Nguyen <poenguyen@Poes-MacBook-Pro.local>
2017-08-02 15:43:04 +0700

    Test trigger automated build

--- voting/app.py
+++ voting/app.py
@@ -9,8 +9,8 @@ import logging
 import signal
 import sys
 
-optionA = "Dev"
-optionB = "Ops"
+optionA = "Devops"
+optionB = "DevOnly"
 name = optionA + " VS " + optionB
 
 redis = Redis(host="redis", db=0)
